57d5d9b659dae37a67afa0d08407deb3dfd70fef
Krishn Parasar <76171905+Krishn1412@users.noreply.github.com>
2024-08-30 10:40:56 +0530

    first commit
    Basic front end for homepage

---
+++ app/layout.tsx
@@ -0,0 +1,32 @@
+import type { Metadata } from "next";
+import Navbar from "@/components/Navbar";
+import { Inter, Space_Grotesk } from "next/font/google";
+import "./globals.css";
+
+const inter = Inter({ subsets: ["latin"] });
+const spaceGrotesk = Space_Grotesk({
+  subsets: ['latin'],
+  weight: ['300', '400', '500', '600', '700']
+})
+
+export const metadata: Metadata = {
+  title: "Scapy",
+  description: "Track product prices effectively",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>
+        <main className="max-w-10xl mx-auto">
+        <Navbar/>
+        {children}
+        </main>
+        </body>
+    </html>
+  );
+}
